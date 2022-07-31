10f41d7dbe113ccf2bfa41a294ce2871bce1f3e0
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-07-31 16:05:07 -0300

    adicionando as rotas de post

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,21 @@
 import { Router } from "express";
 
-import UserController from "./controllers/userController";
+import UserController from "./controllers/UserController";
+import PostController from "./controllers/PostController";
 
 const router = Router();
 
+// user
 router.get("/users", UserController.findAllUsers);
 router.get("/user/:id", UserController.findUser);
 router.post("/user", UserController.createUser);
 router.put("/user/:id", UserController.updateUser);
 router.delete("/user/:id", UserController.deleteUser);
+
+// post
+router.get("/post/user", PostController.findAllPosts);
+router.post("/post/user/:id", PostController.createPost);
+router.put("/post/:id", PostController.updatePost);
+router.delete("/post/:id", PostController.deletePost);
 
 export { router };
